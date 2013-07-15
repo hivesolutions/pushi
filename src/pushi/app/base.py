45f382b984da3def52690d5cec30a1b5bf238317
joamag <joamag@gmail.com>
2013-07-15 20:08:39 +0100

    new hello route

--- src/pushi/app/base.py
+++ src/pushi/app/base.py
@@ -4,9 +4,13 @@
 import appier
 
 class PushiApp(appier.App):
-    
+
     def __init__(self):
         appier.App.__init__(self, name = "pushi")
+
+    @appier.route("^/api/hello$")
+    def tobias(self):
+        return dict(message = "hello world")
 
 if __name__ == "__main__":
     app = PushiApp()

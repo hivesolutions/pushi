7e8c0410644cc4103239bc1c4198201d9ece353e
joamag <joamag@gmail.com>
2013-07-15 18:30:48 +0100

    new init files

--- src/pushi/app/__init__.py
+++ src/pushi/app/__init__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import base
+import request
+
+from base import *
+from request import *
--- src/pushi/server/__init__.py
+++ src/pushi/server/__init__.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import base
+import echo
+import ws
+
+from base import *
+from echo import *
+from ws import *

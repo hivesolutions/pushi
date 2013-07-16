3a9af4ed8279f09a1d8417469b48156a8b66a6bf
joamag <joamag@gmail.com>
2013-07-16 12:07:56 +0100

    support for the pushi ws protocol

--- src/test/pushi.js
+++ src/test/pushi.js
@@ -1,18 +1,76 @@
+var Pushi = function(appKey, options) {
+    var URL = "ws://localhost:9090/";
+    var self = this;
+
+    this.appKey = appKey;
+    this.options = options || {};
+    this.socket = new WebSocket(URL);
+    this.socketId = null;
+    this.state = "disconnected";
+
+    this.socket.onopen = function() {
+    };
+
+    this.socket.onmessage = function(event) {
+        var message = event.data;
+        var json = JSON.parse(message);
+
+        if (self.state == "disconnected"
+                && json.event == "pusher:connection_established") {
+            var data = JSON.parse(json.data);
+            self.socketId = data.socket_id;
+            self.state = "connected";
+            self.onoconnect();
+        } else if (self.state == "connected") {
+            self.onmessage(json);
+        }
+    };
+
+    this.socket.onclose = function() {
+        self.socketId = null;
+        self.state == "disconnected";
+        self.onodisconnect();
+    };
+};
+
+Pushi.prototype.onoconnect = function() {
+    this.subscribe("global");
+};
+
+Pushi.prototype.onodisconnect = function() {
+};
+
+Pushi.prototype.onsubscribe = function(channel) {
+    console.info(channel);
+};
+
+Pushi.prototype.onmessage = function(json) {
+    switch (json.event) {
+        case "pusher_internal:subscription_succeeded" :
+            this.onsubscribe(json.channel);
+            break;
+    }
+};
+
+Pushi.prototype.send = function(json) {
+    var data = JSON.stringify(json);
+    this.socket.send(data);
+};
+
+Pushi.prototype.sendEvent = function(event, data) {
+    var json = {
+        event : event,
+        data : data
+    };
+    this.send(json);
+};
+
+Pushi.prototype.subscribe = function(channel) {
+    this.sendEvent("pusher:subscribe", {
+                channel : channel
+            });
+};
+
 jQuery(document).ready(function() {
-            var socket = new WebSocket("ws://localhost:9090/");
-            
-            socket.onopen = function() {
-                socket.send("Hello World");
-                console.info("Message is sent...");
-            };
-
-            socket.onmessage = function(event) {
-                var message = event.data;
-                console.info("Message is received '" +  message + "'...");
-            };
-
-            socket.onclose = function() {
-                // websocket is closed.
-                console.info("Connection is closed...");
-            };
+            var pushi = new Pushi("asdasd");
         });

a5f3e233a0a65c3b1773f12647dfb00a42db4175
joamag <joamag@gmail.com>
2013-07-16 10:59:13 +0100

    new publish test

--- src/pushi/client/base.py
+++ src/pushi/client/base.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import appier
 import websocket
 
-def start():
+def app_test():
+    payload = dict(
+        name = "dummy",
+        data = "hello world",
+        channels = ["global"]
+    )
+    
+    appier.post("http://localhost:8080/apps/hello/events", data_j = payload)
+
+def ws_test():
     ws = websocket.create_connection("ws://localhost:9090/")
     print "Sending 'Hello, World'..."
     ws.send("Hello, World")
@@ -12,3 +22,6 @@ def start():
     result =  ws.recv()
     print "Received '%s'" % result
     ws.close()
+
+if __name__ == "__main__":
+    app_test()

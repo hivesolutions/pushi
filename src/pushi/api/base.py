9127fd8ee4a064315efd395d00701f74f8831de2
joamag <joamag@gmail.com>
2013-10-01 18:49:28 +0100

    new code comments in base configuration

--- src/pushi/api/base.py
+++ src/pushi/api/base.py
@@ -69,13 +69,28 @@ def authenticate(channel, socket_id, app_key = None, app_secret = None):
     digest = structure.hexdigest()
     return "%s:%s" % (key, digest)
 
+def ensure_login(app_id = None, app_key = None, app_secret = None):
+    global token
+    if token: return token
+    return login(
+        app_id = app_id,
+        app_key = app_key,
+        app_secret = app_secret
+    )
+
 def login(app_id = None, app_key = None, app_secret = None):
     global token
 
+    # retrieves the base pushi related variable either from
+    # the passed arguments or from the global environment
     id = app_id or os.environ["PUSHI_ID"]
     key = app_key or os.environ["PUSHI_KEY"]
     secret = app_secret or os.environ["PUSHI_SECRET"]
 
+    # tries to login in the pushi infra-structure using the
+    # login route together with the full set of auth info
+    # retrieving the result map that should contain the
+    # session token, to be used in further calls
     result = appier.get(BASE_URL + "/login", dict(
         app_id = id,
         app_key = key,
@@ -87,24 +102,22 @@ def login(app_id = None, app_key = None, app_secret = None):
     token = result["token"]
     return token
 
-def ensure_login(app_id = None, app_key = None, app_secret = None):
-    global token
-    if token: return token
-    return login(
-        app_id = app_id,
-        app_key = app_key,
-        app_secret = app_secret
-    )
-
 def trigger(channel, data, event = "message", app_id = None, app_key = None, app_secret = None):
+    # retrieve the app id value either from the passed attributes
+    # or from the global environment variable
     id = app_id or os.environ["PUSHI_ID"]
 
+    # runs the ensure login call making sure that the login token
+    # is currently present in the environment, this is required
+    # to perform secured remote calls
     token = ensure_login(
         app_id = app_id,
         app_key = app_key,
         app_secret = app_secret
     )
 
+    # performs the concrete event trigger operation creating an event
+    # with the provided information using a secure channel
     result = appier.post(BASE_URL + "/apps/%s/events" % app_id, dict(
         data = data,
         event = event,

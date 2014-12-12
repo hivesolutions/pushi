6362f39a4148bb72de5818bfe6c36f96b411eb01
joamag <joamag@gmail.com>
2014-12-12 17:15:52 +0000

    new version

--- lib/setup.py
+++ lib/setup.py
@@ -41,7 +41,7 @@ import setuptools
 
 setuptools.setup(
     name = "pushi",
-    version = "0.2.9",
+    version = "0.2.10",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Pushi System",

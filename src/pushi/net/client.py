c451dd60dd268574f38086c78fd04a2e8e4e5919
joamag <joamag@hive.pt>
2016-04-26 20:06:29 +0100

    new timer support

--- src/pushi/net/client.py
+++ src/pushi/net/client.py
@@ -342,9 +342,16 @@ if __name__ == "__main__":
         print("Received %d event(s) for channel %s" % (len(events), name))
 
     def on_subscribe(channel, data):
+        client = channel.owner.owner
+
+        def timer():
+            print("Waiting for events(s) on channel %s ..." % channel.name)
+            client.delay(timer, timeout = 3)
+
         channel.send("message", "Hello World", persist = False)
         channel.bind("message", on_message)
         channel.latest(count = 20, callback = on_latest)
+        client.delay(timer, timeout = 3)
 
     def on_connect(connection):
         connection.subscribe_pushi("global", callback = on_subscribe)

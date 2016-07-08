a511d3531cf2f7e358594f3f3ce34b9a8caaa75b
joamag <joamag@hive.pt>
2016-07-08 16:27:04 +0100

    removed pre save condition

--- src/pushi/app/models/event.py
+++ src/pushi/app/models/event.py
@@ -85,7 +85,3 @@ class PushiEvent(base.PushiBase):
     @classmethod
     def list_names(cls):
         return ["mid", "channel", "owner_id", "timestamp"]
-
-    def pre_save(self):
-        base.PushiBase.pre_save(self)
-        appier.verify(not "mid" in self.data)
